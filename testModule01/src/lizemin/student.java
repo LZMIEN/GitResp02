01bf7cdd72748dd8d7aafdf44457da2b19273382
lizemin <15871812940@163.com>
2020-08-05 18:36:05 +0800

    删除了注释,看是否会报错

--- testModule01/src/lizemin/student.java
+++ testModule01/src/lizemin/student.java
@@ -1,13 +1,6 @@
 package lizemin;
 
 import sun.security.mscapi.PRNG;
-
-/**
- * @Author:Lizm
- * @Date: 2020/8/5
- * @Description: TestGit
- * @Version: 2.0
- */
 public class student {
     private int age; //
     private String name ;//
